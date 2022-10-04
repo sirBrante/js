250c80476a4cfa1c280dfbbf06e3db05899f9115
sirBrante <112952207+sirBrante@users.noreply.github.com>
2022-10-04 18:12:14 +0500

    add files

---
+++ primJs.js
@@ -0,0 +1,2 @@
+a="background-color:#00ffff; color:#ff00ff;" a+="font-size:24pt; font-family:'Times New Roman'" naim='Сеть магазинов "ВСЁ ДЛЯ ДОМА"'
+let da=new Date() d=da.getDate()+"."+(da.getMonth()+1)+"."+da.getFullYear() document.write('<P align=center style= "'+a+'">'+ naim+'</p><P>Сегодня '+d+'</p>')
